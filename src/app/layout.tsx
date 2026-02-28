7acaff60f79ee7b24783fedd2e4f8c1ce33b6989
Yes-Amrit <amugi2712@gmail.com>
2026-02-28 21:48:36 +0530

    feat: implement user synchronization and dashboard page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css'; 
 import { ClerkProvider } from '@clerk/nextjs'
+import UserSync from "@/components/UserSync";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,8 +42,8 @@ export default function RootLayout({
       >
       <html lang="en" className="dark">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-          >
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <UserSync />
           {children}
         </body>
       </html>
